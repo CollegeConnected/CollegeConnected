7fa710463dd456192f8370356cb477c9140d8481
WaseemBaraz <waseem.baraz@gmail.com>
2016-10-12 19:09:38 -0400

    Created a button that goes to a basic student form where students enter in their information.

---
+++ CollegeConnected/Controllers/StudentCreatorController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CollegeConnected.Models;
+
+namespace CollegeConnected.Controllers
+{
+    public class StudentCreatorController : Controller
+    {
+        [HttpGet]
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult Index(StudentCreator eventCreator)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Your Information has now been updated!", eventCreator);
+            }
+            else
+            {
+                //there is a validation error
+                return View();
+            }
+        }
+    }
+}
