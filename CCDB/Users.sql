7b04a7a462456a85233d420d5fec71c5e74a9baa
Jared Magaro <brandonmagaro@gmail.com>
2016-11-08 16:42:44 -0500

    I changed and updated the Login and Index views to be fitting for Admin Login and I inserted Admin Login Credentials into dbo.Users

--- CCDB/Users.sql
+++ CCDB/Users.sql
@@ -1,10 +1,6 @@
-﻿GO
-CREATE TABLE [dbo].[Users]
+﻿CREATE TABLE [dbo].[Users]
 (
 	[UserID] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
     [Username] VARCHAR(100) NOT NULL, 
-    [Password] VARBINARY(50) NULL
-);
-
-
-INSERT INTO [dbo].[Users] (Username, Password) VALUES ('unf-alumni@unf.edu','password');
+    [Password] VARCHAR(50) NOT NULL
+)
