39717c10f0c980d7be8b1a407b448e3c9f409fc3
Jared Magaro <brandonmagaro@gmail.com>
2016-11-08 15:46:24 -0500

    I changed the view to be fitting for Admin Login

--- CCDB/Users.sql
+++ CCDB/Users.sql
@@ -1,6 +1,10 @@
-﻿CREATE TABLE [dbo].[Users]
+﻿GO
+CREATE TABLE [dbo].[Users]
 (
 	[UserID] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
     [Username] VARCHAR(100) NOT NULL, 
     [Password] VARBINARY(50) NULL
-)
+);
+
+
+INSERT INTO [dbo].[Users] (Username, Password) VALUES ('unf-alumni@unf.edu','password');
