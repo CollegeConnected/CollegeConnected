60eaf8042c4f0a2b90a9de8a17a53a6925319041
bennr9 <rachel.bennett16@yahoo.com>
2016-11-10 00:41:33 -0500

    EventHome is now working, I added a new model to go along with it. Home/Index view looks better. Removed Register button from navbar.

---
+++ CollegeConnected/Models/HomePageModels.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CollegeConnected.Models
+
+{
+    public class HomePage
+    {
+        [Required(ErrorMessage = "Please enter your student N number")]
+        public string StudentNumber { get; set; }
+
+        [Required(ErrorMessage = "Please enter your first Name")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Please enter your last Name")]
+        public string LastName { get; set; }
+    }
+}
