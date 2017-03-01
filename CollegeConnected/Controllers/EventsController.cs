8f6ef822f1974262593c0df9ef0adc21e97043bb
hunter <hunter@DESKTOP-7G7EJI9>
2017-02-28 20:01:29 -0500

    Fixed some cosmetic stuff. Added some of code required to register during an event.

--- CollegeConnected/Controllers/EventsController.cs
+++ CollegeConnected/Controllers/EventsController.cs
@@ -12,14 +12,10 @@ namespace CollegeConnected.Controllers
     public class EventsController : Controller
     {
         private readonly CollegeConnectedDbContext db = new CollegeConnectedDbContext();
-
-        // GET: Events http://stackoverflow.com/questions/18237945/how-to-edit-save-viewmodels-data-back-to-database
         public ActionResult Index()
         {
             return View(db.Events.ToList());
         }
-
-        // GET: Events/Details/5
         public ActionResult Details(Guid? id)
         {
             if (id == null)
@@ -29,16 +25,11 @@ namespace CollegeConnected.Controllers
                 return HttpNotFound();
             return View(@event);
         }
-
-        // GET: Events/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: Events/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create(
@@ -56,8 +47,7 @@ namespace CollegeConnected.Controllers
 
             return View(@event);
         }
-
-        // GET: Events/Edit/5
+        
         public ActionResult Edit(Guid? id)
         {
             if (id == null)
@@ -67,10 +57,6 @@ namespace CollegeConnected.Controllers
                 return HttpNotFound();
             return View(@event);
         }
-
-        // POST: Events/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit(
@@ -84,30 +70,6 @@ namespace CollegeConnected.Controllers
             }
             return View(@event);
         }
-
-        // GET: Events/Delete/5
-        public ActionResult Delete(Guid? id)
-        {
-            if (id == null)
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            var @event = db.Events.Find(id);
-            if (@event == null)
-                return HttpNotFound();
-            return View(@event);
-        }
-
-        // POST: Events/Delete/5
-        [HttpPost]
-        [ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(Guid id)
-        {
-            var @event = db.Events.Find(id);
-            db.Events.Remove(@event);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
         protected override void Dispose(bool disposing)
         {
             if (disposing)
@@ -133,6 +95,9 @@ namespace CollegeConnected.Controllers
 
         public ActionResult SignIn(Guid id)
         {
+            var ccEvent = db.Events.Find(id);
+            string eventTitle = ccEvent.EventName;
+            ViewBag.Title = eventTitle;
             string searchString = "";
             var studentList = (from student in db.Students
                                where student.StudentNumber == searchString
@@ -168,7 +133,6 @@ namespace CollegeConnected.Controllers
                 return View(studentList);
             }
         }
-        // GET: Students/Edit/5
         public ActionResult Confirm(Guid? id, Guid? eventId)
         {
             Student student = db.Students.Single(x => x.StudentId == id);
@@ -180,10 +144,7 @@ namespace CollegeConnected.Controllers
                 return HttpNotFound();
              return View(eventViewModel);
         }
-
-        // POST: Students/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Confirm(
